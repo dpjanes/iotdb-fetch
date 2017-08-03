af3f211133313be850746520c6cc8fea75643614
David Janes <davidjanes@davidjanes.com>
2017-08-03 15:41:44 -0400

    reorg

--- DIST.sh
+++ DIST.sh
@@ -35,6 +35,7 @@ echo "=================="
         README.md LICENSE \
         package.json \
         index.js \
+        lib/*.js \
         |
     ( cd "${NPM_DST}" && tar xvf - && npm publish ) || exit 1
     git commit -m "new release" package.json || exit 1
